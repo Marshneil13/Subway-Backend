332f39f63de0bf13babee3e43e495879b6b83e1c
marshnil_13 <btech10225.20@bitmesra.ac.in>
2023-01-16 23:18:45 +0530

    backend authentication on user login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,11 +5,26 @@ const User = require("../models/userModel");
 
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  const newUser = new User({ name, email, password });
+  const newUser = await new User({ name, email, password });
 
   try {
     newUser.save();
     res.send("User Registered Successfully!");
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.find({ email, password });
+    if (user.length > 0) {
+      res.send("User logged in successfully");
+    } else {
+      return res.status(400).json({ message: "User login failed" });
+    }
   } catch (error) {
     return res.status(400).json({ message: error });
   }
