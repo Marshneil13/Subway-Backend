f6fcbd36e7978a4cf40873871fa14690e8fd5ba0
marshnil_13 <btech10225.20@bitmesra.ac.in>
2023-05-12 20:51:24 +0530

    add new sub logic

--- routes/subwaysRoute.js
+++ routes/subwaysRoute.js
@@ -13,4 +13,24 @@ router.get("/getallsubways", async (req, res) => {
   }
 });
 
+router.post("/addsubways", async (req, res) => {
+  const subway = req.body.subway;
+
+  try {
+    const newSubway = new Subway({
+      name: subway.name,
+      varients: ["six inch", "foot-long"],
+      prices: [subway.prices],
+      category: subway.category,
+      description: subway.description,
+      image: subway.image,
+      icon: subway.icon,
+    });
+    await newSubway.save();
+    res.send("New Sub added Successfully");
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+});
+
 module.exports = router;
