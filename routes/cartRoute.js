589a723a383f16570ae38cfe12fc43f266b50050
marshnil_13 <btech10225.20@bitmesra.ac.in>
2023-05-23 13:16:53 +0530

    final commit

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -2,4 +2,37 @@ const express = require("express");
 const router = express.Router();
 const Cart = require("../models/cartModel");
 
+router.post("/getusercart", async (req, res) => {
+  const { userEmail } = req.body;
+
+  try {
+    const cart = await Cart.findOne({ email: userEmail });
+    if (cart) {
+      const cartItems = cart.cartItems;
+      res.send(cartItems);
+    } else {
+      return res.status(400).json({ message: "Unable to fetch Cart" });
+    }
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+});
+
+router.post("/updateusercart", async (req, res) => {
+  const { cartItems, userEmail } = req.body;
+
+  try {
+    const cart = await Cart.findOne({ email: userEmail });
+    if (cart) {
+      cart.cartItems = cartItems;
+      await cart.save();
+      res.json({message: "Updated cart!"})
+    } else {
+      return res.status(400).json({ message: "Couldn't update user cart" });
+    }
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+});
+
 module.exports = router;
